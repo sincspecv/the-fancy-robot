25fcb9852a4173e6eb958947f16f5d1ddf3df39e
Matthew Schroeter <schroeter.m83@gmail.com>
2021-06-22 16:36:06 -0500

    basic footer is styled

--- wp-content/themes/the-fancy-robot/resources/views/partials/footer.blade.php
+++ wp-content/themes/the-fancy-robot/resources/views/partials/footer.blade.php
@@ -1,4 +1,16 @@
 <footer class="footer" id="colophon">
+  <div class="grid-container">
+    <div class="grid-x grid-margin-x grid-margin-y align-middle">
+      <div class="cell shrink align-left">
+        <p class="copyright">&copy; {!! date('Y') !!} The Fancy Robot, LLC</p>
+      </div>
+      <div class="cell medium-auto align-right">
+        @if(has_nav_menu('footer_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav grid-x align-right footer-nav', 'walker' => new App\Navigation\Foundation(),]) !!}
+        @endif
+      </div>
+    </div>
+  </div>
   <div class="modal" id="cal-modal">
     <div class="modal-container">
       <div class="modal-close" aria-label="close popup" role="button">&#127335;</div>
