0a3fd0823aef1bd4eff2f8031e9e64ef56a90cb7
Matthew Schroeter <schroeter.m83@gmail.com>
2021-07-06 16:53:53 -0500

    styled archive page

--- wp-content/themes/the-fancy-robot/resources/views/index.blade.php
+++ wp-content/themes/the-fancy-robot/resources/views/index.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-  <div class="grid-container">
+{{--  @include('partials.page-header')--}}
+  <div class="grid-container module">
+    <div class="grid-x grid-margin-x grid-margin-y">
+      <header class="cell auto">
+        <h1>Blog</h1>
+      </header>
+    </div>
     <div class="grid-x grid-margin-x grid-margin-y">
       @if (!have_posts())
         <div class="cell small-12 module">
--- wp-content/themes/the-fancy-robot/resources/views/partials/content.blade.php
+++ wp-content/themes/the-fancy-robot/resources/views/partials/content.blade.php
@@ -1,9 +1,23 @@
-<article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
+<div class="cell medium-4">
+  <div class="card">
+    <div class="card-section archive-image">
+      <figure style="background-image:url({!! esc_url_raw($featured_image->url) !!})">
+        <img src="{!! esc_url_raw($featured_image->url) !!}" alt="{!! esc_attr($featured_image->alt) !!}" class="show-for-sr" />
+      </figure>
+    </div>
+    <div class="card-section archive-content">
+      <article @php post_class() @endphp>
+        <header>
+          <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+          @include('partials/entry-meta')
+        </header>
+        <div class="entry-summary">
+          @php the_excerpt() @endphp
+        </div>
+      </article>
+    </div>
+    <div class="card-section archive-button">
+      <a href="{!! get_permalink() !!}" class="button primary">Read More</a>
+    </div>
   </div>
-</article>
+</div>
