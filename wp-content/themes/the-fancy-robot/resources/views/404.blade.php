80cf8e5a31a7609a3f7dd2ca15c2cb2df2d51508
Matthew Schroeter <schroeter.m83@gmail.com>
2021-07-06 16:08:27 -0500

    styled 404 page

--- wp-content/themes/the-fancy-robot/resources/views/404.blade.php
+++ wp-content/themes/the-fancy-robot/resources/views/404.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+  <div class="grid-container four-oh-four-container">
+    <div class="grid-x grid-margin-x grid-margin-y align-center-middle">
+      <div class="cell medium-10 text-center">
+        @if (!have_posts())
+          <h1>404</h1>
+          <div class="alert alert-warning">
+            {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+          </div>
+        @endif
+      </div>
     </div>
-    {!! get_search_form(false) !!}
-  @endif
+  </div>
 @endsection
--- wp-content/themes/the-fancy-robot/resources/views/index.blade.php
+++ wp-content/themes/the-fancy-robot/resources/views/index.blade.php
@@ -2,17 +2,22 @@
 
 @section('content')
   @include('partials.page-header')
+  <div class="grid-container">
+    <div class="grid-x grid-margin-x grid-margin-y">
+      @if (!have_posts())
+        <div class="cell small-12 module">
+          <div class="alert alert-warning">
+            {{ __('Sorry, no results were found.', 'sage') }}
+          </div>
+          {!! get_search_form(false) !!}
+        </div>
+      @endif
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+      @while (have_posts()) @php the_post() @endphp
+        @include('partials.content-'.get_post_type())
+      @endwhile
+
+      {!! App::pagination() !!}
     </div>
-    {!! get_search_form(false) !!}
-  @endif
-
-  @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
-  {!! App::pagination() !!}
+  </div>
 @endsection
